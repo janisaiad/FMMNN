94438a5e83458d0f0169facae0b7338d26b8d077
janisaiad <janisaiad.ja@gmail.com>
2025-07-19 20:30:55 -0400

    small subtil thing in the init.py file of model, need to compare to github.com/janisaiad/SCIML but everything's working

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
-from .mmnn import MMNN
-from .mmnn_jax import MMNNJax
+from .mmnn.mmnn import MMNN
+from .mmnn.mmnn_jax import MMNNJax
 from .mmnn.fmmnn import FMMNN
 
 __all__ = ["MMNN", "MMNNJax", "FMMNN"]
